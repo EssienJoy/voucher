fb674de0fea211c2780eea36f12082787e97b6c8
essienjoy <ejoy7602@gmail.com>
2026-09-08 22:01:38 +0100

    added some web url protective middlewares

--- voucher_backend/src/app.ts
+++ voucher_backend/src/app.ts
@@ -1,17 +1,60 @@
 import express, { type Express } from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+// import path from 'path';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import errorController from './controller/errorController.js';
 import businessRouter from './routes/businessRoutes.js';
 import voucherRouter from './routes/voucherRoutes.js';
 import AppError from '../utils/appError.js';
 
-// console.log(new Error());
-// const error = new Error();
-// console.log(error.name);
-
 const app: Express = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://voucher-ashy.vercel.app/'],
+    credentials: true,
+  }),
+);
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: { policy: 'cross-origin' },
+  }),
+);
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'development') {
+  //logs information about incoming requests
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this Ip, please try again in 1 hour',
+});
+
+app.use('/api', limiter);
+
+// Modifies incoming requests
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+app.use(compression());
+
+if (process.env.NODE_ENV === 'development') {
+  //logs information about incoming requests
+  app.use(morgan('dev'));
+}
+
 app.use(express.static('public'));
 app.use('/api/v1/user', businessRouter);
 app.use('/api/v1/voucher', voucherRouter);
