67185c41a7cfa57d621390404059c43a18e09bbb
essienjoy <ejoy7602@gmail.com>
2026-09-10 17:21:33 +0100

    fixed typescript build issues

--- voucher_backend/dist/model/voucherModel.d.ts
+++ voucher_backend/dist/model/voucherModel.d.ts
@@ -1,62 +1,5 @@
 import mongoose from 'mongoose';
-declare const Voucher: mongoose.Model<{
-    business_id: mongoose.Types.ObjectId;
-    code: string;
-    title: string;
-    description?: string | null;
-    discount_type: "fixed" | "percentage";
-    discount_value: number;
-    usage_limit: number;
-    min_purchase?: number | null;
-    max_discount?: number | null;
-    expiry_date: NativeDate;
-    status: "active" | "expired" | "redeemed";
-    redemption_count: number;
-} & mongoose.DefaultTimestampProps, {}, {}, {
-    id: string;
-}, mongoose.Document<unknown, {}, {
-    business_id: mongoose.Types.ObjectId;
-    code: string;
-    title: string;
-    description?: string | null;
-    discount_type: "fixed" | "percentage";
-    discount_value: number;
-    usage_limit: number;
-    min_purchase?: number | null;
-    max_discount?: number | null;
-    expiry_date: NativeDate;
-    status: "active" | "expired" | "redeemed";
-    redemption_count: number;
-} & mongoose.DefaultTimestampProps, {
-    id: string;
-}, {
-    toJSON: {
-        virtuals: true;
-    };
-    toObject: {
-        virtuals: true;
-    };
-    timestamps: true;
-}> & Omit<{
-    business_id: mongoose.Types.ObjectId;
-    code: string;
-    title: string;
-    description?: string | null;
-    discount_type: "fixed" | "percentage";
-    discount_value: number;
-    usage_limit: number;
-    min_purchase?: number | null;
-    max_discount?: number | null;
-    expiry_date: NativeDate;
-    status: "active" | "expired" | "redeemed";
-    redemption_count: number;
-} & mongoose.DefaultTimestampProps & {
-    _id: mongoose.Types.ObjectId;
-} & {
-    __v: number;
-}, "id"> & mongoose.HydratedDocumentOverrides<{
-    id: string;
-}>, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, {
+declare const voucherSchema: mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, {
     toJSON: {
         virtuals: true;
     };
@@ -138,9 +81,9 @@ declare const Voucher: mongoose.Model<{
     _id: mongoose.Types.ObjectId;
 } & {
     __v: number;
-}>, {
-    createdAt: NativeDate;
-    updatedAt: NativeDate;
+}>;
+export type VoucherDocument = mongoose.HydratedDocument<mongoose.InferSchemaType<typeof voucherSchema>>;
+declare const Voucher: mongoose.Model<{
     business_id: mongoose.Types.ObjectId;
     code: string;
     title: string;
@@ -153,10 +96,51 @@ declare const Voucher: mongoose.Model<{
     expiry_date: NativeDate;
     status: "active" | "expired" | "redeemed";
     redemption_count: number;
-} & {
+} & mongoose.DefaultTimestampProps, {}, {}, {}, mongoose.Document<unknown, {}, {
+    business_id: mongoose.Types.ObjectId;
+    code: string;
+    title: string;
+    description?: string | null;
+    discount_type: "fixed" | "percentage";
+    discount_value: number;
+    usage_limit: number;
+    min_purchase?: number | null;
+    max_discount?: number | null;
+    expiry_date: NativeDate;
+    status: "active" | "expired" | "redeemed";
+    redemption_count: number;
+} & mongoose.DefaultTimestampProps, {}, mongoose.DefaultSchemaOptions> & {
+    business_id: mongoose.Types.ObjectId;
+    code: string;
+    title: string;
+    description?: string | null;
+    discount_type: "fixed" | "percentage";
+    discount_value: number;
+    usage_limit: number;
+    min_purchase?: number | null;
+    max_discount?: number | null;
+    expiry_date: NativeDate;
+    status: "active" | "expired" | "redeemed";
+    redemption_count: number;
+} & mongoose.DefaultTimestampProps & {
     _id: mongoose.Types.ObjectId;
 } & {
     __v: number;
-}>;
+} & {
+    id: string;
+}, any, {
+    business_id: mongoose.Types.ObjectId;
+    code: string;
+    title: string;
+    description?: string | null;
+    discount_type: "fixed" | "percentage";
+    discount_value: number;
+    usage_limit: number;
+    min_purchase?: number | null;
+    max_discount?: number | null;
+    expiry_date: NativeDate;
+    status: "active" | "expired" | "redeemed";
+    redemption_count: number;
+} & mongoose.DefaultTimestampProps>;
 export default Voucher;
 //# sourceMappingURL=voucherModel.d.ts.map
