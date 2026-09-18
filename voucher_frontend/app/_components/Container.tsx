d008959a8d674872db8149ab4a94a299e18fa349
Essien Joy <ejoy7602@gmail.com>
2026-09-18 08:50:06 +0100

    Update padding in Container component

--- voucher_frontend/app/_components/Container.tsx
+++ voucher_frontend/app/_components/Container.tsx
@@ -1,9 +1,10 @@
+import type {componentProps} from "react";
 import { cn } from "../_lib/utils";
 
 type ContainerProps = React.ComponentProps<"div">;
 
 const Container = ({ className, ...props }: ContainerProps) => {
-  return <div className={cn("px-3 max-w-7xl", className)} {...props} />;
+  return <div className={cn("px-20 max-w-7xl", className)} {...props} />;
 };
 
 export default Container;
